504e5783f4de907a69c8565ff861bb4c2b88d5f6
Rafael Díaz de León <leon@elinter.net>
2018-10-29 19:23:04 -0600

    Fixed bug at serving the ball after point (#2)
    After scoring a point, the ball now restarts at the center at
    the base speed without at angle at the direction of the player
    that the point was scored against.

--- src/netpong.c
+++ src/netpong.c
@@ -10,6 +10,10 @@
 #define SCREEN_HEIGHT 600
 #define SCREEN_NAME "NetPong"
 
+#define BASE_BALL_SPEED 4
+#define CENTER_X (SCREEN_WIDTH / 2)
+#define CENTER_Y (SCREEN_HEIGHT / 2)
+
 void game_init();
 void game_quit();
 
@@ -153,21 +157,25 @@ void check_events(int *moving)
 
 void check_collisions(SDL_Rect p1_rect, SDL_Rect p2_rect, SDL_Rect *ball_rect, int *ball_speed)
 {
-    int start = Game.screen.width / 2;
     static float angle = 0;
 
     SDL_bool c1 = SDL_HasIntersection(ball_rect, &p1_rect);
     SDL_bool c2 = SDL_HasIntersection(ball_rect, &p2_rect);
 
 
-
     if (ball_rect->x < 0){
         printf("POINT to player 2!\n");
-        ball_rect->x = start;
+        ball_rect->x = CENTER_X;
+        ball_rect->y = CENTER_Y;
+        angle = 0;
+        *ball_speed = BASE_BALL_SPEED * -1;
     } else if(ball_rect->x > (int)(Game.screen.width - ball_rect->w)) {
         // Change for stop win game
         printf("POINT to player 1!\n");
-        ball_rect->x = start;
+        ball_rect->x = CENTER_X;
+        ball_rect->y = CENTER_Y;
+        angle = 0;
+        *ball_speed = BASE_BALL_SPEED;
     }
 
     if (c1 || c2) {
@@ -195,8 +203,8 @@ int main()
 
     SDL_Texture *ball_texture = load_image("images/ball.png");
     SDL_Rect ball_rect = {
-        Game.screen.width / 2,
-        Game.screen.height / 2,
+        CENTER_X,
+        CENTER_Y,
         32,
         32
     };
@@ -204,13 +212,13 @@ int main()
     SDL_Texture *paddle_texture = load_image("images/paddle.png");
     SDL_Rect p1_rect = {
         32,
-        Game.screen.height / 2 - 64,
+        CENTER_Y - 64,
         32,
         128
     };
     SDL_Rect p2_rect = {
         Game.screen.width - 64,
-        Game.screen.height / 2 - 64,
+        CENTER_Y - 64,
         32,
         128
     };
