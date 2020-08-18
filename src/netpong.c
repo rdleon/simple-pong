718d410c901fd6763320fe86379199713af6f13d
ozkar <ozkar@ozkar.org>
2020-08-18 00:20:40 -0700

    adds angle calculation

--- src/netpong.c
+++ src/netpong.c
@@ -153,13 +153,13 @@ void reset_ball(SDL_Rect* ball_rect, int* ball_speed, float* angle, int directio
 }
 
 struct direction_vector
-check_collisions(SDL_Rect p1_rect, SDL_Rect p2_rect, SDL_Rect *ball_rect, int *ball_speed)
+check_collisions(SDL_Rect *p1_rect, SDL_Rect *p2_rect, SDL_Rect *ball_rect, int *ball_speed)
 {
     static float angle = 0;
     struct direction_vector direction;
 
-    SDL_bool c1 = SDL_HasIntersection(ball_rect, &p1_rect);
-    SDL_bool c2 = SDL_HasIntersection(ball_rect, &p2_rect);
+    SDL_bool c1 = SDL_HasIntersection(ball_rect, p1_rect);
+    SDL_bool c2 = SDL_HasIntersection(ball_rect, p2_rect);
 
     if (ball_rect->x < 0){
         Game.score2++;
@@ -174,12 +174,12 @@ check_collisions(SDL_Rect p1_rect, SDL_Rect p2_rect, SDL_Rect *ball_rect, int *b
         if (*ball_speed < 0) {
             *ball_speed *= -1;
         }
-        angle = (rand() % 3) - 1;
+        angle = calculate_angle(p1_rect, ball_rect);
     } else if (c2) {
         if (*ball_speed > 0) {
             *ball_speed *= -1;
         }
-        angle = (rand() % 3) - 1;
+        angle = calculate_angle(p2_rect, ball_rect);
     }
 
     if (ball_rect->y < 0 || ball_rect->y > (int)(Game.screen.height - ball_rect->h)) {
@@ -212,6 +212,34 @@ void follow_ball(SDL_Rect *ball, SDL_Rect *paddle)
         }
         paddle->y -= BASE_PADDLE_SPEED;
     }
+}
+
+// calculate angle to return ball on paddle collision.
+// this breaks the paddle into 3 segments (up, middle, down)
+// and changes the angle depending on where the ball collisioned
+// if the ball hits the middle segment, it randomly selects the angle.
+int calculate_angle(SDL_Rect *paddle, SDL_Rect *ball)
+{
+    int third = (paddle->h / 3);
+
+    int middle_start = paddle->y + third;
+    int middle_end = paddle->y + third*2;
+
+    int paddle_middle = paddle->y + (paddle->h /2); // this breaks paddle in two
+
+    if (ball-y >= middle_start && ball->y <= middle_end) {
+        // if collision is in the "middle" segment
+        // randomly pick one of the 3 values.
+        return (rand() % 3) - 1;
+    } else if (ball->y > paddle_middle) {
+        // collision is in the upper segment
+        return 1;
+    } else {
+        // collision is in the lower segment
+        return -1;
+    }
+
+    return 0;
 }
 
 Uint32 frame_limit(Uint32 last_tick, const Uint32 frame_limit) {
@@ -282,7 +310,7 @@ int main()
         // moving is the speed and direction of the paddle
         check_events(keyboardState, &moving);
 
-        direction = check_collisions(p1_rect, p2_rect, &ball_rect, &ball_speed);
+        direction = check_collisions(&p1_rect, &p2_rect, &ball_rect, &ball_speed);
 
         ball_rect.x = direction.x;
         ball_rect.y = direction.y;
