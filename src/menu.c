9adb1cd61a75e12c02be0307023f6bf6a95aded3
Rafael Diaz de Leon <r@rdleon.me>
2020-10-11 18:21:51 -0500

    Change the capitalization of the game name

--- src/menu.c
+++ src/menu.c
@@ -7,7 +7,7 @@ void menu_loop(const Uint8 *keyboard_state)
     SDL_RenderClear(Game.screen.renderer);
     SDL_RenderCopy(Game.screen.renderer, Game.textures.background, NULL, NULL);
 
-    draw_text(Game.screen.renderer, "NETPONG", SCREEN_WIDTH / 3, 30, 5);
+    draw_text(Game.screen.renderer, "NetPong", SCREEN_WIDTH / 3, 30, 5);
     draw_text(Game.screen.renderer, "Press ENTER to start", SCREEN_WIDTH / 3, 260, 2);
 
     if (keyboard_state[SDL_SCANCODE_RETURN]) {
