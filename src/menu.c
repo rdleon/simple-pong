790acd0fc3ffbf2f95f7e3737a75539dc5a37ffd
Rafael Diaz de Leon <leon@elinter.net>
2020-10-15 18:45:53 -0500

    Add message for Q going back to main menu

--- src/menu.c
+++ src/menu.c
@@ -12,8 +12,9 @@ void menu_controls_loop(const Uint8 *keyboard_state)
 
     draw_text(Game.screen.renderer, "UP: Move up", SCREEN_WIDTH / 4, 160, 3);
     draw_text(Game.screen.renderer, "DOWN: Move down", SCREEN_WIDTH / 4, 230, 3);
+    draw_text(Game.screen.renderer, "Q: Go back to menu", SCREEN_WIDTH / 4, 300, 3);
 
-    if (keyboard_state[SDL_SCANCODE_RETURN]) {
+    if (keyboard_state[SDL_SCANCODE_RETURN] || keyboard_state[SDL_SCANCODE_Q]) {
         Game.state = Menu;
         SDL_Delay(DEBOUNCE_WAIT);
     }
