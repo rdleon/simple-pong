9c43a574eb7cb2bf2dc4ef208b62dd21d0a4cddd
Rafael Diaz de Leon <r@rdleon.me>
2023-06-16 18:12:04 -0500

    Add P2 to Options menu
    Add P2 to options menu, improved spacing and prepare for
    configurable keys

--- src/config_controls.c
+++ src/config_controls.c
@@ -4,14 +4,22 @@ extern struct game Game;
 
 void menu_controls_loop(const Uint8 *keyboard_state)
 {
+    int quarter_screen = SCREEN_WIDTH / 4;
+    int half_screen = SCREEN_WIDTH / 2;
     SDL_RenderClear(Game.screen.renderer);
     SDL_RenderCopy(Game.screen.renderer, Game.textures.background, NULL, NULL);
 
-    draw_text(Game.screen.renderer, "Configuration", SCREEN_WIDTH / 3, 30, 5);
+    draw_text(Game.screen.renderer, "Configuration", SCREEN_WIDTH / 5, 30, 5);
 
-    draw_text(Game.screen.renderer, "UP: Move up", SCREEN_WIDTH / 4, 160, 3);
-    draw_text(Game.screen.renderer, "DOWN: Move down", SCREEN_WIDTH / 4, 230, 3);
-    draw_text(Game.screen.renderer, "Q: Go back to menu", SCREEN_WIDTH / 4, 300, 3);
+    draw_text(Game.screen.renderer, "P1 UP:", quarter_screen, 160, 2);
+    draw_text(Game.screen.renderer, "Arrow UP", half_screen, 160, 2);
+    draw_text(Game.screen.renderer, "P1 DOWN:", quarter_screen, 200, 2);
+    draw_text(Game.screen.renderer, "Arrow DOWN", half_screen, 200, 2);
+    draw_text(Game.screen.renderer, "P2 UP:", quarter_screen, 280, 2);
+    draw_text(Game.screen.renderer, "O", half_screen, 280, 2);
+    draw_text(Game.screen.renderer, "P2 DOWN:", quarter_screen, 320, 2);
+    draw_text(Game.screen.renderer, "L", half_screen, 320, 2);
+    draw_text(Game.screen.renderer, "Q: Go back to menu", quarter_screen, 400, 2);
 
     if (keyboard_state[SDL_SCANCODE_RETURN] || keyboard_state[SDL_SCANCODE_Q]) {
         Game.state = Menu;
